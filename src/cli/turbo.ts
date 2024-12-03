5bb08cfa20b8ca0543c4fd9194cbfcdd4b959e86
HyperionU <108021531+HyperionU@users.noreply.github.com>
2024-12-03 04:26:13 +0000

    add turbo prompt

--- src/cli/turbo.ts
+++ src/cli/turbo.ts
@@ -3,6 +3,7 @@ import { PackageManager } from "~/utils/getPackageManager.js";
 import * as prompt from "@clack/prompts"
 import gradient from "gradient-string";
 import { setTimeout } from "timers/promises";
+import { isEmpty } from "~/utils/checkDir.js";
 
 export const turboCLI = async (packageManager:PackageManager) => {
     switch (packageManager) {
@@ -19,11 +20,34 @@ export const turboCLI = async (packageManager:PackageManager) => {
             break;
     }
 
-    await turboPrompt();
+    const turboPath = await turboPrompt();
 
+    switch (packageManager) {
+        case "npm":
+            await execa({stdout: 'inherit', stderr: 'inherit'})`npx create-turbo ${turboPath} -m ${packageManager}`
+            break;
+    
+        default:
+            await execa({stdout: 'inherit', stderr: 'inherit'})`${packageManager} dlx create-turbo ${turboPath} -m ${packageManager}`
+            break;
+    }
 }
 
 const turboPrompt = async () => {
     prompt.note(`Welcome to ${gradient.passion("Turbo")}. Let's get started.`, "Step 2.")
     await setTimeout(1000)
+
+    const turbo = await prompt.text({
+        message: "What is the path to your new site?",
+        placeholder: "./turbo",
+        validate: (value) => {
+            if (!value) return 'Please enter a path.';
+            if (value[0] !== '.') return 'Please enter a relative path.';
+            if (!isEmpty(value)) return 'Please enter an empty path.';
+            return;
+        }
+    }) as string;
+
+    return turbo;
+    
 }
