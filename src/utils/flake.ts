d87cd2f1a44a7af6c6eba7ecff8db30cdf8ac18d
HyperionU <108021531+HyperionU@users.noreply.github.com>
2024-12-15 23:34:40 +0000

    add flake validation

---
+++ src/utils/flake.ts
@@ -0,0 +1,17 @@
+import Ajv from "ajv";
+import * as schema from "../../flake.schema.json"
+
+const ajv = new Ajv();
+
+const validate = ajv.compile(schema);
+
+export const flakeValidate = (data: any) => {
+
+    const valid = validate(data);
+
+    if (!valid) {
+        return console.error(validate.errors)
+    }
+    
+    return console.log("Data Valid!")
+}
